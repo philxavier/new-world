516517281fc6ec620478c62abfc12d1d83a72649
Felipe <phil.sousa@gmail.com>
2019-04-23 15:25:08 -0700

    initial commit - map is rendere

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SimpleMap from './SimpleMap.jsx'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SimpleMap />
+        this is a test
+      </div>
+    )
+  }
+}
